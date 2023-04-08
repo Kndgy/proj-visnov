8825cc78d3381c90d920edf3221485f37396098d
kaine <ghanirafli8@gmail.com>
2023-04-08 21:24:02 +0700

    handle file uploads, and create configbutton

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,36 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
+const UploadImage = () => {
+  const [image, setImage] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if(file){
+        setImage(file);
+    }
+  };
+
+  const handleUpload = () => {
+    if (image) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const dataUrl = reader.result as string;
+        localStorage.setItem('image', dataUrl);
+      };
+      reader.readAsDataURL(image);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} /> 
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+};
+
+
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
@@ -21,19 +51,10 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
+          <button type="submit">Create Config</button>
+          <br/>
+          <br/>
+          <UploadImage/>
       </div>
       <p>{greetMsg}</p>
     </div>
