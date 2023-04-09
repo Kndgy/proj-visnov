8a6bcf8ee6dbea33286eade5fac948877b07fb9d
kaine <ghanirafli8@gmail.com>
2023-04-09 13:49:34 +0700

    config file read and write functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,101 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
-type ImageData = {
-    image: string;
-};  
-
-const initialCollections: ImageData[] = []
+interface configInterface {
+    ProjDir: string;
+    imageCollections: { image: string }[];
+}
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
+    const [fileStructures, setFileStructure] = useState();
 
-  const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
-  const [imgCollection, setImgCollection] = useState<ImageData[]>(initialCollections)
-
-  const UploadImage = () => {
-    const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files && e.target.files[0];
-        if(file){
-            setSelectedFile(file);
-        }
-    };
+    useEffect(() => {
+        console.log(fileStructures);
+    }, [fileStructures]);
     
-    const handleUpload = () => {
-        if (selectedFile) {
-        const reader = new FileReader();
-        reader.readAsDataURL(selectedFile);
-        reader.onloadend = () => {
-            const dataUrl = reader.result as string;
-            const structure = 
-            {
-                image: dataUrl
-            }
-            const imgStructures = [...imgCollection, structure]
-            setImgCollection(imgStructures)
-            console.log(imgStructures)
-            localStorage.setItem('image', JSON.stringify(imgStructures));
-        };
-      }
-    };
-
-    const colls = localStorage.getItem('image')
-    let parsedImages: ImageData[] = []
-    if(colls){
-        parsedImages = JSON.parse(colls)
+    async function readFile() {
+        try {
+            const filecontent = await invoke('readJsonFile', {filePath:'D:/lol.txt'});
+            setFileStructure(JSON.parse(filecontent as string));
+        } catch (error) {
+            console.error(error);
+        }
     }
-    console.log(parsedImages)
 
-    const renderImage = () => {
-      if(colls){
-        return(
-            <>{parsedImages.map((item)=><img src={item.image}/>)}</>
-        )
-      }
-      return null;
+    if(!fileStructures){
+        readFile()
+    }
+
+    const defaultconf= `       
+        {
+            "ProjDir":"test",
+            "imageCollections":[
+                {
+                    "image":""
+                }
+            ]
+        }
+    `
+
+    async function saveConfig(content:string) {
+        invoke('saveBlankFile', {filename:"lol", content:content});
+    }
+      
+    const UploadImage = () => {
+        const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
+        const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+            const file = e.target.files && e.target.files[0];
+            if(file){
+                setSelectedFile(file);
+            }
+            
+        };
+
+        const handleUpload = () => {
+            if (selectedFile) {
+                const reader = new FileReader();
+                reader.readAsDataURL(selectedFile);
+                reader.onloadend = () => {
+                    const dataUrl = reader.result as string;
+                    const structure = 
+                    {
+                        image: dataUrl
+                    }
+                    if(fileStructures){
+                        const configStructure: configInterface = fileStructures;
+                        configStructure.imageCollections.push(structure)
+                        saveConfig(JSON.stringify(configStructure))
+                        console.log("image", configStructure.imageCollections)
+                    }
+                };
+            }
+        };
+
+        const renderImage = () => {
+
+        };
+
+        return (
+            <div>
+                <input type="file" multiple onChange={handleFileInput} />
+                <button onClick={handleUpload}>Upload</button>
+                <p/>
+                {/* <div className="rendered-image">{renderImage()}</div> */}
+            </div>
+        );
     };
-
-    return (
-      <div>
-        <input type="file" multiple onChange={handleFileInput} />
-        <button onClick={handleUpload}>Upload</button>
-        <div className="rendered-image">{renderImage()}</div>
-      </div>
-    );
-};
-
-  async function greet() {
-    // invoke('saveBlankFile', {filename:"lol", content:`
-    //     [
-    //         {
-    //             "ProjDir":"test"
-    //         }
-    //     ]`
-    // });
-  }
-
-  async function readFile() {
-    await invoke('readJsonFile', {filePath:'D:/lol.json'}).then((filecontent)=>{
-        console.log(filecontent)
-    }).catch((error) => {
-        console.error(error);
-    });
-  }
 
   return (
     <div className="container">
       <div className="row">
-          <button onClick={greet} type="submit">Create Config</button>
+          {/* <button onClick={saveConfig} type="submit">Create Config</button> */}
           <p/>
           <button onClick={readFile} type="submit">Log file content</button>
           <p/>
           <UploadImage/>
       </div>
-      <p>{greetMsg}</p>
     </div>
   );
 }
