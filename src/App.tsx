069c5f3813a8a1e27dcc0750afd4a557519bd925
kaine <ghanirafli8@gmail.com>
2023-04-15 21:36:31 +0700

    fix indents

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import "./App.css";
 import { PagesModule } from "./pagesModule";
 
 interface configInterface {
-    ProjDir: string;
-    imageCollections: { image: string }[];
+  ProjDir: string;
+  imageCollections: { image: string }[];
 }
 
 function App() {
-    const [fileStructures, setFileStructure] = useState();
-    const [currentPage, setCurrentPage] = useState(0);
+  const [fileStructures, setFileStructure] = useState();
+  const [currentPage, setCurrentPage] = useState(0);
 
-    const defaultconf= `       
+  const defaultconf = `       
     {
         "ProjDir":"test",
         "imageCollections":[
@@ -23,103 +23,116 @@ function App() {
             }
         ]
     }
-`
+`;
 
-async function saveConfig(content:string) {
-    invoke('saveBlankFile', {filename:"lol", content:content});
-}
+  async function saveConfig(content: string) {
+    invoke("saveBlankFile", { filename: "lol", content: content });
+  }
 
-    useEffect(() => {
-    }, [fileStructures]);
-    async function readFile() {
-        try {
-            const filecontent = await invoke('readJsonFile', {filePath:'D:/lol.txt'});
-            setFileStructure(JSON.parse(filecontent as string));
-            console.log(fileStructures)
-        } catch (error) {
-            saveConfig(defaultconf)
-            console.error("error", error);
-        }
+  useEffect(() => {}, [fileStructures]);
+  async function readFile() {
+    try {
+      const filecontent = await invoke("readJsonFile", {
+        filePath: "D:/lol.txt",
+      });
+      setFileStructure(JSON.parse(filecontent as string));
+      console.log(fileStructures);
+    } catch (error) {
+      saveConfig(defaultconf);
+      console.error("error", error);
     }
+  }
 
-    if(!fileStructures){
-        readFile()
-    }
-      
-    const UploadImage = () => {
-        const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
-        const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-            const file = e.target.files && e.target.files[0];
-            if(file){
-                setSelectedFile(file);
-            }
-            
-        };
+  if (!fileStructures) {
+    readFile();
+  }
 
-        const handleUpload = () => {
-            if (selectedFile) {
-                const reader = new FileReader();
-                reader.readAsDataURL(selectedFile);
-                reader.onloadend = () => {
-                    const dataUrl = reader.result as string;
-                    const structure = 
-                    {
-                        page:0,
-                        image: dataUrl
-                    }
-                    if(fileStructures){
-                        const configStructure: configInterface = fileStructures;
-                        configStructure.imageCollections.push(structure)
-                        saveConfig(JSON.stringify(configStructure))
-                        console.log("image", configStructure.imageCollections)
-                    }
-                };
-            }
-        };
-
-        const renderImage = () => {
-            if(fileStructures){
-                const configStructure: configInterface = fileStructures;
-                saveConfig(JSON.stringify(configStructure))
-                return(
-                    <div>{configStructure.imageCollections.map((items, index)=><img key={index} src={items.image}/>)}</div>
-                )
-            }
-        };
-
-        return (
-            <div>
-                <input type="file" accept="image/*" multiple onChange={handleFileInput} />
-                <button onClick={handleUpload}>Upload</button>
-                <p/>
-                <div className="rendered-image">{renderImage()}</div>
-            </div>
-        );
+  const UploadImage = () => {
+    const [selectedFile, setSelectedFile] = useState<File | undefined>(
+      undefined
+    );
+    const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files && e.target.files[0];
+      if (file) {
+        setSelectedFile(file);
+      }
     };
 
-    const handleNext = () => {
-        setCurrentPage(currentPage+1)
-    }
+    const handleUpload = () => {
+      if (selectedFile) {
+        const reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend = () => {
+          const dataUrl = reader.result as string;
+          const structure = {
+            page: 0,
+            image: dataUrl,
+          };
+          if (fileStructures) {
+            const configStructure: configInterface = fileStructures;
+            configStructure.imageCollections.push(structure);
+            saveConfig(JSON.stringify(configStructure));
+            console.log("image", configStructure.imageCollections);
+          }
+        };
+      }
+    };
 
-    const handlePrev = () => {
-        setCurrentPage(currentPage-1)
-    }
-
-    console.log(currentPage)
+    const renderImage = () => {
+      if (fileStructures) {
+        const configStructure: configInterface = fileStructures;
+        saveConfig(JSON.stringify(configStructure));
+        return (
+          <div>
+            {configStructure.imageCollections.map((items, index) => (
+              <img key={index} src={items.image} />
+            ))}
+          </div>
+        );
+      }
+    };
 
     return (
-        <div className="container">
-            <div className="row">
-                <PagesModule/>
-                {/* <button onClick={saveConfig} type="submit">Create Config</button> */}
-                <p/>
-                <button onClick={readFile} type="submit">Log file content</button>
-                <p/>
-                <button onClick={handlePrev}>previous page</button> <button onClick={handleNext}>next page</button>
-                {/* <UploadImage/> */}
-            </div>
-        </div>
+      <div>
+        <input
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={handleFileInput}
+        />
+        <button onClick={handleUpload}>Upload</button>
+        <p />
+        <div className="rendered-image">{renderImage()}</div>
+      </div>
     );
+  };
+
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePrev = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  console.log(currentPage);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <PagesModule />
+        {/* <button onClick={saveConfig} type="submit">Create Config</button> */}
+        <p />
+        <button onClick={readFile} type="submit">
+          Log file content
+        </button>
+        <p />
+        <button onClick={handlePrev}>previous page</button>{" "}
+        <button onClick={handleNext}>next page</button>
+        {/* <UploadImage/> */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
